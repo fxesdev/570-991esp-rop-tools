00548c3345ef703c8f67b326c7638c72c7d7eb55
u68 <78559138+u68@users.noreply.github.com>
2024-01-02 20:12:50 +1300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+chmod +x ./gadget-to-hex.py
+chmod +x ./trowel.py
+chmod +x ./help.py
+chmod +x ./roptools.py
+printf '\e[1;35mfinished setting up!\n'
+echo 'TERMINAL WILL SELF DESTRUCT IN 5 SECONDS TO APPLY CHANGES'
+sleep 1
+echo '4'
+sleep 1
+echo '3'
+sleep 1
+echo '2'
+sleep 1
+echo '1'
+sleep 1
+echo 'did you actually think that would happen? lol!'
+sleep 10
+echo 'this device will crash soon!'
+sleep 1
+echo c /proc/sysrq-trigger
